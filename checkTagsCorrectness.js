44198b7e963e6d18798fba5bfff0f64c06c98a3b
a4455395 <a4455395@gmail.com>
2018-09-27 14:44:19 +1200

    first

---
+++ checkTagsCorrectness.js
@@ -0,0 +1,45 @@
+const getTagName = tag =>  tag.match(/([^\/<>]+)/)[0];
+
+const isOpenedTag = tag => !/\//.test(tag);
+
+const checkTagsCorrectness = string => {
+    //extract tags from string
+    let tagArr = string.match(/(<\/?[A-Z]+>)/g);
+    let current = 0;
+    while (current < tagArr.length - 1) {
+        //check if first tag is closed
+        if(!isOpenedTag(tagArr[current])) {
+            return 'expected # found ' + tagArr[current];
+        }
+        //check if tags are pair (open + closed)
+        if(isOpenedTag(tagArr[current]) && !isOpenedTag(tagArr[current + 1])) {
+            //check if tag names are matched
+            const tagName1 = getTagName(tagArr[current]),
+                tagName2 = getTagName(tagArr[current+1]);
+            if(tagName1 === tagName2) {
+                tagArr.splice(current,2);
+                if(current) {
+                    current--;
+                    continue;
+                }
+                //if tags are mismatched => return negative result
+            } else {
+                return `Expected </${tagName1}> found </${tagName2}>`
+            }
+        }
+        current++;
+    }
+
+    if(tagArr.length === 1) {
+        const tag = tagArr[0],
+            tagName = getTagName(tag);
+        if(isOpenedTag(tag)) {
+            return `Expected </${tagName}> found #`;
+        } else {
+            return `Expected # found ${tag}`;
+        }
+    }
+    return 'Correctly tagged paragraph';
+};
+
+module.exports = checkTagsCorrectness;
---
+++ index.js
@@ -0,0 +1,19 @@
+const checkTagsCorrectness = require('./checkTagsCorrectness');
+
+const strings = [
+    'The following text<C><B>is centred and in boldface</B></C>',
+    '<B>This <\\g>is <B>boldface</B> in <<*> a</B> <\\6> <<d>sentence',
+    '<B><C> This should be centred and in boldface, but the\n' +
+    'tags are wrongly nested </B></C>',
+    '<B>This should be in boldface, but there is an extra closing\n' +
+    'tag</B></C>',
+    '<B><C>This should be centred and in boldface, but there is\n' +
+    'a missing closing tag</C>'
+];
+
+strings.forEach(s => console.log(checkTagsCorrectness(s)));
+
+
+
+
+
